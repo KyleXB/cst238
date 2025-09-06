158e32cf6572715df4401718fb5204e3f960039c
Kyle Bulloch <kyle.x.bulloch@gmail.com>
2025-09-05 21:50:29 -0700

    week 2 homework start

---
+++ src/Student.java
@@ -0,0 +1,26 @@
+/*
+ * Title: HW02a_Quiz.java
+ * Abstract: This program reads in multiple students, their IDs, and their test scores. It then calculates and
+ *           prints the average of each students' test scores.
+ * Author: Kyle Bulloch
+ * Email: kbulloch@csumb.edu
+ * Estimate: 2 hours
+ * Date: 9/5/2025
+ */
+public class Student {
+    private String name;
+    private int id;
+    private double [] quizScores = new double [5];
+    private double averageScore;
+
+    public Student(String nm, int idNum) {
+        name = nm;
+        id = idNum;
+    }
+
+    public void setScore(double [] score) {
+
+    }
+
+
+}
