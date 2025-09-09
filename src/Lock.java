46d0c19fd9baa609fc54fa8afeefa20924599b49
Kyle Bulloch <kyle.x.bulloch@gmail.com>
2025-09-08 21:28:58 -0700

    week 2 full finish

--- src/Lock.java
+++ src/Lock.java
@@ -18,6 +18,7 @@ public class Lock {
     private Lock(int code1, int code2, int code3) {
         codes = new int[] {code1, code2, code3};
         currentCode = guessCount = 0;
+        allGuesses = new int[120];
     }
 
     public boolean isLocked() {
@@ -25,15 +26,34 @@ public class Lock {
     }
 
     public void guess(int guessNum) {
+        allGuesses[guessCount] =  guessNum;
+        guessCount++;
 
+        if (guessNum == codes[currentCode]) {
+            System.out.println("Correct!");
+            currentCode++;
+        } else if (guessNum > codes[currentCode]) {
+            System.out.println("Go down");
+        } else if (guessNum < codes[currentCode]) {
+            System.out.println("Go up");
+        } else {
+            System.out.println("Number is out of range, please enter a number between 1 and 40, inclusive.");
+        }
     }
 
     public String guesses() {
-        return "placeholder";
+        StringBuilder sb = new StringBuilder();
+        sb.append(guessCount).append(" guesses: ");
+
+        for (int i = 0; i < guessCount; i++) {
+            sb.append(allGuesses[i]).append(" ");
+        }
+
+        return sb.toString();
     }
 
     public String toString() {
-        return "placeholder";
+        return "The codes are: " + codes[0] + " " + codes[1] + " " + codes[2];
     }
 
     public static void main(String[] args) {
