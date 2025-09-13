c22c3dc5fd1e68a7d5db30a9979a5af754f4cbfc
Kyle Bulloch <kyle.x.bulloch@gmail.com>
2025-09-13 10:03:20 -0700

    week 3 homework student complete

---
+++ src/Course.java
@@ -0,0 +1,11 @@
+/*
+ * Title: Course.java
+ * Abstract: This class acts as a school course, containing the course number, it's title, the instructor, and the students currently enrolled in it.
+ * Author: Kyle Bulloch
+ * Email: kbulloch@csumb.edu
+ * Estimate: 1.5 hours
+ * Date: 9/13/2025
+ */
+public class Course {
+
+}
