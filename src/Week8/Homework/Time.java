87670dfeb1ea8b8f3dc23dcc725068a4eb123974
Kyle Bulloch <kyle.x.bulloch@gmail.com>
2025-10-17 22:57:48 -0700

    week 8 part b start

--- src/Week8/Homework/Time.java
+++ src/Week8/Homework/Time.java
@@ -1,4 +1,33 @@
 package Week8.Homework;
 
 public class Time /*implements Comparable<Time>*/{
+    private int hours;
+    private int minutes;
+
+    public Time() {
+        this.hours = 0;
+        this.minutes = 0;
+    }
+
+    public Time(int hours) {
+        this.hours = hours;
+        this.minutes = 0;
+
+        if (hours < 0 || hours > 23) {
+            this.hours = 0;
+            this.minutes = 0;
+        }
+    }
+
+    public Time(int hours, int minutes) {
+        if (hours < 0 || hours > 23 || minutes < 0 || minutes > 59) {
+            this.hours = 0;
+            this.minutes = 0;
+        } else {
+            this.hours = hours;
+            this.minutes = minutes;
+        }
+    }
+
+
 }
