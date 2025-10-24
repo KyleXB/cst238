829c68e6926cb7ad5a37b49bb13d63bc63720266
bull4189 <kbulloch@csumb.edu>
2025-10-24 11:05:40 -0700

    week 9 friday food partial

--- src/Week9/FridayLab/Food.java
+++ src/Week9/FridayLab/Food.java
@@ -1,4 +1,32 @@
 package Week9.FridayLab;
 
 public class Food {
+    private String name;
+    private double price;
+    private int calories;
+
+    public Food(String name, double price, int calories) {
+        this.name = name;
+        this.price = price;
+        this.calories = calories;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+    public int getCalories() {
+        return calories;
+    }
+
+    public int getPriceInCents() {
+        return (int) (price * 100);
+    }
+
+    public String toString(){
+        return String.format("%s $%.2f %d calories", name, price, calories);
+    }
 }
