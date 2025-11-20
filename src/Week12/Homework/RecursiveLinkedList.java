c9019f67a150d943300883673968c143a19a1cf0
Kyle Bulloch <kyle.x.bulloch@gmail.com>
2025-11-19 19:36:56 -0800

    week 12 homework partial 2

--- src/Week12/Homework/RecursiveLinkedList.java
+++ src/Week12/Homework/RecursiveLinkedList.java
@@ -1,7 +1,5 @@
 package Week12.Homework;
 
-import Week12.FridayLab.Node;
-
 /*
  * Title: RecursiveLinkedList.java
  * Abstract: The java file contains a Linked List class that has various recursive methods, and a Node class that manages items in a given Linked List.
@@ -39,10 +37,9 @@ public class RecursiveLinkedList <T extends Comparable<T>> {
 
     public RecursiveLinkedList<T> append(T value) {
         if (isEmpty()) {
-            System.out.println("Empty List");
-            return new RecursiveLinkedList<>();
+            first = new Node(value);
+            return this;
         }
-
         first.append(value);
         return this;
     }
@@ -66,16 +63,37 @@ public class RecursiveLinkedList <T extends Comparable<T>> {
     public RecursiveLinkedList<T> getDistinct() {
         RecursiveLinkedList<T> list = new RecursiveLinkedList<>();
         if (!isEmpty()) { first.addIfNotPresent(list); } //as long as list is not empty, getDistinct can pass onto addIfNotPresent
-
         return list;
     }
 
+    /*
+        There is no direction here on whether or not we can assume that isSorted takes in a distinct array;
+        as such I am coding it such that if there are non-distinct values in the array, it'll simply move to the remaining values.
+     */
     public boolean isSorted() {
-        return false;
+        if (isEmpty() || first.getNext() == null) { return true; } //empty list and singular value list is, by default, "sorted"
+
+        int compareValue = first.getData().compareTo(first.getNext().getData());
+        if (compareValue < 0) {
+            return first.isSortedAscending();
+        } else if (compareValue > 0) {
+            return first.isSortedDescending();
+        } else {
+            RecursiveLinkedList<T> subList = new RecursiveLinkedList<>();
+            subList.first = first.getNext();
+            return subList.isSorted();
+        }
     }
 
     public void insertOrderedAscending(T value) {
-
+        if (isEmpty()) {
+            first = new Node(value);
+            return;
+        } else if (first.getData().compareTo(value) <= 0) {
+            prepend(value);
+        } else {
+            first.insertOrderedAscending(value);
+        }
     }
 
 
@@ -161,15 +179,24 @@ public class RecursiveLinkedList <T extends Comparable<T>> {
         }
 
         public boolean isSortedAscending() {
-            return false;
+            if (next == null) { return true; }
+            if (data.compareTo(next.getData()) > 0) { return false;} //value a > value b; therefore false for ascending
+            return next.isSortedAscending();
         }
 
         public boolean isSortedDescending() {
-            return false;
+            if (next == null) { return true; }
+            if (data.compareTo(next.getData()) < 0) { return false;} //value a < value b; therefore false for descending
+            return next.isSortedDescending();
         }
 
         public void insertOrderedAscending(T value) {
+            if (next == null) { this.setNext(new Node(value)); }
+            if (next.getData().compareTo(value) >= 0) {
+                this.setNext(new Node(value));
+            }
 
+            return;
         }
 
         public void moveToEnd(Node n) {
